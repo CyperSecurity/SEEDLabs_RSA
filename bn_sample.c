d75578612e9544a961cfea748990bd4ad877b9a6
Abdullah <abdallah.elsaid25@gmail.com>
2025-01-09 08:40:50 -0500

    Add basic operation

--- bn_sample.c
+++ bn_sample.c
@@ -7,21 +7,23 @@ void printBN(char *msg, BIGNUM *a);
 
 int main(void)
 {
+  BN_CTX *ctx = BN_CTX_new();
   BIGNUM *a = BN_new();
+  BIGNUM *b = BN_new();
+  BIGNUM *n = BN_new();
+  BIGNUM *res = BN_new();
 
-  // Assign a value from a decimal number string
-  BN_dec2bn(&a, "12345678901112231223");
+  BN_generate_prime_ex(a, NBITS, 1, NULL, NULL, NULL);
+  BN_dec2bn(&b, "273489463796838501848592769467194369268");
+  BN_rand(n, NBITS, 0, 0);
 
-  // Assign a value from a hex number string
-  // BN_hex2bn(&a, "2A3B4C55FF77889AED3F");
+  // res = a*b
+  BN_mul(res, a, b, ctx);
+  printBN("a * b = ", res);
 
-  // Generate a random number of 128 bits
-  // BN_rand(a, 128, 0, 0);
-
-  // Generate a random prime number of 128 bits
-  // BN_generate_prime_ex(a, 128, 1, NULL, NULL, NULL);
-
-  printBN("This is my big num", a);
+  // res = aˆb mod n
+  BN_mod_exp(res, a, b, n, ctx);
+  printBN("aˆc mod n = ", res);
   return 0;
 }
 
