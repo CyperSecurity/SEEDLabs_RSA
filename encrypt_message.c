d9f3c6793850f1e234149d734f5ae0e0dbd87847
Abdullah <abdallah.elsaid25@gmail.com>
2025-01-11 09:53:05 -0500

    Fix e value in the encryption_message.c

--- encrypt_message.c
+++ encrypt_message.c
@@ -22,9 +22,9 @@ int main(void)
   BN_CTX *ctx = BN_CTX_new(); // Define ctx for calculations.
 
   // Assign values.
-  BN_hex2bn(&m,"E85CED54AF57E53E092113E62F436F4F"); 
+  BN_hex2bn(&m,"4120746f702073656372656421"); 
   BN_hex2bn(&n,"DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5"); 
-  BN_bin2bn("010001", 6, e); 
+  BN_hex2bn(&e, "010001"); 
   BN_hex2bn(&d,"74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D"); 
 
   BN_mod_exp(encrypted_msg, m, e, n, ctx); 
